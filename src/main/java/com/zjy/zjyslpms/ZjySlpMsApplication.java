93ade6c95c789f40512754bcad7e6a4ca4a77aa5
zhangjy <zhangjy@163.cn>
2025-01-13 17:20:18 +0800

    梦开始的地方

---
+++ src/main/java/com/zjy/zjyslpms/ZjySlpMsApplication.java
@@ -0,0 +1,13 @@
+package com.zjy.zjyslpms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZjySlpMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZjySlpMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zjy/zjyslpms/ZjySlpMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjy.zjyslpms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZjySlpMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
